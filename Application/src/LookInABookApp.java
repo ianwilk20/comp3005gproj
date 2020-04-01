4aebab35a8f2a2070dfa49434b2f705cafa86f03
Ian <ianwilk20@gmail.com>
2020-03-31 23:06:57 -0400

    Added Basic Prompt Loop Functionality and Workflow Path

--- Application/src/LookInABookApp.java
+++ Application/src/LookInABookApp.java
@@ -10,51 +10,101 @@ public class LookInABookApp {
         String password = "rootPass";
 
         Connection c = null;
+
       try {
          Class.forName("org.postgresql.Driver");
          c = DriverManager
             .getConnection(url, user, password);
-        System.out.println("Connected to the PostgreSQL server successfully.\n\n");
-        System.out.print("Enter a course to find prerequisites for: ");
-        Scanner uInput = new Scanner(System.in);
-        String course = uInput.nextLine();
-        System.out.println("");
-        Statement statement = c.createStatement();
-        HashMap<String, String> preReqs = new HashMap<String, String>();
-        Boolean morePreReqsExist = true;
-        while (morePreReqsExist){
-          try{
-            ResultSet qResult = statement.executeQuery("select prereq.prereq_id" + 
-                                                       " from course inner join prereq" + 
-                                                       " on course.course_id = prereq.course_id" +
-                                                       " where course.course_id = '" + course + "'");
-
-            if (!qResult.next()){
-              System.out.println("There is no prerequisite for " + course); 
-              morePreReqsExist = false;
-            } else {
-                if (preReqs.containsKey(course)) {
-                  morePreReqsExist = false;
-                } else {
-                  preReqs.put(course, qResult.getString("prereq_id"));
-                  System.out.println("The prerequisite for " + course + " is: " + qResult.getString("prereq_id"));  
-                  course = qResult.getString("prereq_id");
-                }
-              qResult.close();
-            }
-            
-          } catch (SQLException sqlE) {
-            sqlE.printStackTrace();
-            System.err.println(sqlE.getClass().getName()+": "+sqlE.getMessage());
-            System.exit(0);
-          }
-        }
-
       } catch (Exception e) {
-         e.printStackTrace();
-         System.err.println(e.getClass().getName()+": "+e.getMessage());
-         System.exit(0);
+          e.printStackTrace();
+          System.err.println(e.getClass().getName()+": "+e.getMessage());
+          System.exit(0);
       }
-      System.out.println("\nThank you for using our database prerequiste querying system");
+
+        System.out.println("Connected to the PostgreSQL server successfully.\n\n");
+        startLoop(c);
+    }
+
+    private static void startLoop(Connection c){
+        boolean uoLoop = true;
+        while(uoLoop){
+            System.out.println("Would you like to login as a User or Owner?\n\nPress (1) for User \t\t Press (2) for Owner\n\n \t\t Selection: ");
+            Scanner uInput = new Scanner(System.in);
+            Integer role = uInput.nextInt();
+
+            if (role == 1){ //User
+                uoLoop = false;
+                userLoop(c);
+            } else if (role == 2){ //Owner
+                uoLoop = false;
+                ownerLoop(c);
+            } else {
+                System.out.println("Invalid selection");
+                continue;
+            }
+        }
+    }
+
+    private static void userLoop(Connection c) {
+        System.out.println("Would you like to Login or Create and Account?\n\nPress (1) to Login \t\t Press (2) to Create an Account\n\n \t\t Selection: ");
+        Scanner uInput = new Scanner(System.in);
+        Integer role = uInput.nextInt();
+        if (role == 1){
+            System.out.println("Please enter your username: ");
+            uInput = new Scanner(System.in);
+            String user_name = uInput.nextLine();
+            Boolean userInDB = getUserWithUsername(c, user_name);
+            if (userInDB){ System.out.println("Authenticated"); }
+            else {
+                System.out.println("Username not found!");
+                userLoop(c);
+            }
+        }
+        else if (role == 2){
+            System.out.println("Please enter a username to create: ");
+            uInput = new Scanner(System.in);
+            String user_name = uInput.nextLine();
+            //addUserToDB;
+        }
+    }
+
+    private static void ownerLoop(Connection c) {
+        System.out.println("In Owner Loop");
+    }
+
+    private static boolean getUserWithUsername(Connection c, String username){
+        String query = "SELECT count(user_name) FROM users WHERE user_name='" + username + "'";
+        Statement stmt = null;
+        try {
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            rs.next();
+            int count = rs.getInt(1);
+            if (count > 0) { return true; }
+            else { return false; }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * Get Users From DB
+     **/
+    private String getUser(Connection c) {
+        String query = "SELECT * from users";
+        Statement stmt = null;
+        try {
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            rs.next();
+            String acc_no = rs.getString("account_no");
+            String users = rs.getString("user_name");
+            String email = rs.getString("email");
+            return acc_no + " | " + users + " | " + email;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
